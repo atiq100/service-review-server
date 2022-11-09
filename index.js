0366fabba22cd278057a703f2fdada9b3e55161f
atiq100 <atiqur1997rj@gmail.com>
2022-11-09 19:58:03 +0600

    connect database

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port =process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6znnq0v.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('dosofy').collection('services');
+        const reviewCollection = client.db('dosofy').collection('reviews');
+
+        //service api
+        app.get('/services',async(req,res)=>{
+            const query = {} //for get all data
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        //for get service data by specific id
+        app.get('/services/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(err=>console.log(err));
 
 app.get('/',(req,res)=>{
     res.send('doctor service review server is running');

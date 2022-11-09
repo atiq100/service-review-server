da6346ea4449967e1b0706896fe45b090e4f3081
atiq100 <atiqur1997rj@gmail.com>
2022-11-09 20:01:50 +0600

    add service api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port =process.env.PORT || 5000;
 
@@ -19,9 +20,9 @@ async function run(){
 
         //service api
         app.get('/services',async(req,res)=>{
-            const query = {} //for get all data
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
+           const query = {} //for get all data
+           const cursor = serviceCollection.find(query);
+           const services = await cursor.toArray();
             res.send(services);
         })
 
